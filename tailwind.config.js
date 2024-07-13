358a82a1e75f9740e256d7577790b4d5d3a29410
Pavlo Bondarchuk <pasharadio@yahoo.com>
2024-07-13 22:18:53 +0000

    Fix Tailwind CSS content configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/**/*.html", "./src/**/*.css", "./src/**/*.js"],
   theme: {
     extend: {
       screens: {
