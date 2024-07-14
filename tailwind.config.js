1983a3d5b4642baac9de62f0094e4fdc6a52d328
Pavlo Bondarchuk <pasharadio@yahoo.com>
2024-07-14 08:01:54 +0000

    Add color tokens to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,75 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#011C79",
-        buttonBg: "#0237F1",
-        white: "#FFFFFF",
+        neutral: {
+          10: "#F7F7F7",
+          20: "#CCCCCC",
+          50: "#808080",
+          100: "#000000",
+          white: "#FFFFFF",
+        },
+        blue: {
+          10: "#E6F0FB",
+          20: "#CCD7F6",
+          50: "#8098F0",
+          100: "#0237F1",
+          200: "#011C79",
+        },
+        mint: {
+          10: "#EDFFFE",
+          20: "#DBFFFC",
+          50: "#A8FFEF",
+          100: "#4DF4E9",
+          200: "#01A399",
+        },
+        purple: {
+          10: "#F0E7FB",
+          20: "#E1CFFF",
+          50: "#8357EC",
+          100: "#7D0ED9",
+          200: "#320764",
+        },
+        green: {
+          10: "#E7FFEF",
+          20: "#DEFFEA",
+          50: "#A8DBBA",
+          100: "#5BD776",
+          200: "#145222",
+        },
+        yellow: {
+          10: "#FFFEF8",
+          20: "#FFF6D6",
+          50: "#FFEB74",
+          100: "#F4B534",
+          200: "#805500",
+        },
+        red: {
+          10: "#FFFEFF",
+          20: "#FFDFD6",
+          50: "#FF8A9A",
+          100: "#FF5243",
+          200: "#990012",
+        },
+        surface: {
+          primary: "#011C79",
+          secondary: "#FFFFFF",
+          highlight: "#01A399",
+          disabled: "#808080",
+        },
+        content: {
+          primary: "#011C79",
+          secondary: "#FFFFFF",
+          disabled: "#808080",
+        },
       },
       fontFamily: {
         sans: ["Noto Sans", "sans-serif"],
+      },
+      fontSize: {
+        lg: ["20px", "28px"],
+        md: ["16px", "24px"],
+        sm: ["14px", "20px"],
+        xs: ["12px", "16px"],
       },
       screens: {
         mobile: "430px",
