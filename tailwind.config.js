d8d21438c92d25c0601c03aadbd7ae2df448c021
Pavlo Bondarchuk <pasharadio@yahoo.com>
2024-07-14 07:46:30 +0000

    Update header with new design and styles, add color/surface/primary

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,24 @@ module.exports = {
   content: ["./public/**/*.html", "./src/**/*.css", "./src/**/*.js"],
   theme: {
     extend: {
+      colors: {
+        primary: "#011C79",
+        buttonBg: "#0237F1",
+        white: "#FFFFFF",
+      },
+      fontFamily: {
+        sans: ["Noto Sans", "sans-serif"],
+      },
       screens: {
         mobile: "430px",
         tablet: "1024px",
         desktop: "1248px",
+      },
+      boxShadow: {
+        header: "0 0 8px 0 rgba(1, 28, 121, 0.08)",
+      },
+      backdropBlur: {
+        header: "16px",
       },
     },
   },
